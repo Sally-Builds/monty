4551446314d30fe199d14bd64c3d6964169afe12
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-21 16:57:48 -0700

    swap stack top

--- swap.c
+++ swap.c
@@ -11,16 +11,10 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	(void)line_number; 
 
-	if (*stack == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		printf("L%u: can't swap an empty stack\n", line_number);
+		fprintf(stderr, "L%u: can't swap an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
-	}
-
-	if ((*stack)->next == NULL)
-	{
-		printf("L%u: can't swap an empty stack\n", line_number);
-                exit(EXIT_FAILURE);
 	}
 
 	temp = (*stack)->next;
