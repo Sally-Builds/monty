bde557c506500cd4c84eb177e3a1ce7af6f63615
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-22 10:37:33 -0700

    mod and mul

--- monty.h
+++ monty.h
@@ -50,6 +50,8 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void execute_instruction(char *opcode, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
