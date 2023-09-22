affa84d6007c8d6bc2f381b1e38f90c807ee1bfd
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-21 18:52:09 -0700

    debugging

--- add.c
+++ add.c
@@ -14,7 +14,7 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if (current == NULL || current->next == NULL)
 	{
-		printf("L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
