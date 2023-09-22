adecc56bcaa2053667910b9a355df557124d79ef
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-21 19:04:46 -0700

    debuggin

--- push.c
+++ push.c
@@ -13,7 +13,7 @@ void push(stack_t **stack, unsigned int line_number)
 	/**size_t i = 0; */
 	stack_t *new_node;
 
-	token = strtok(NULL, " \t\r\n");
+	token = strtok(NULL, " \n");
 	if (!token)
 	{
 		fprintf(stderr, "L%u usage: push integer\n", line_number);
